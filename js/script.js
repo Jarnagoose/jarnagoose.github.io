105ab5f6b9094f50a6a2fedb4813b5ba3fe61b0b
Chazbc <60560626+Chazbc@users.noreply.github.com>
2024-07-14 11:42:57 -0400

    Redoing everything
    Hopefully, things work now

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    const navbar = document.getElementById("navigationbar");
-    window.addEventListener("scroll", function() {
+
+    window.onscroll = function() {
         if (window.scrollY > 50) {
             scrollToTopBtn.style.display = "block";
-            navbar.style.position = fixed;
-            navbar.style.top = 0;
         } else {
             scrollToTopBtn.style.display = "none";
-            navbar.style.position = absolute;
         }
+    };
+
+    scrollToTopBtn.addEventListener("click", function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 
     const copyToClipboard = (elementId) => {
