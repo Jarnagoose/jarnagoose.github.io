c1cece4a9e89c8a119737a0d0029796b9359fb16
Chazbc <60560626+Chazbc@users.noreply.github.com>
2024-07-24 09:46:27 -0400

    Adding JS and placeholders (and Zach) for team profiles

--- js/script.js
+++ js/script.js
@@ -78,3 +78,52 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+//Rotary Menu for Team Memeber Profiles
+document.addEventListener("DOMContentLoaded", function () {
+    // Get all the radio buttons
+    const radioButtons = document.querySelectorAll('input[name="team"]');
+
+    // Get the dynamic text span
+    const dynamicText = document.getElementById("dynamicText");
+    const dynamicText2 = document.getElementById("dynamicText2");
+    const dynamicImage = document.getElementById("dynamicImage");
+
+    // Define the text to display for each radio button
+    const textMap1 = {
+        A: "Mr. Jarnagoose",
+        B: "Serin",
+        C: "Indefinite",
+        D: "Czbc",
+        E: "Jamie",
+        F: "Maths",
+    };
+    const textMap2 = {
+        A: "*HONK* The creative driving force behind our studio is an enigma, some say he's more goose than man, some say he's more jar than goose. One thing is for sure, he really likes bread crusts and wobbling around.",
+        B: "Serin has been programming games for fun since he was just a little line of code, Serin was one of the first to join the flock, and has been here since the very beginning.",
+        C: "Indefinite wears many hats, besides translating the crayon etched ravings of Mr. Jarnagoose into something coherent, Indefinite also serves to keep everyone focused. If anyone deserves a medal, it's him.",
+        D: "Somehow holding the entire art department together is one talented individual, CzBC. Legend says 18 years ago he taught Jarnagoose how to sprite his very first piece of art on the computer. Legend also says it was kinda bad.",
+        E: "Be prepared to have your socks blown off by the musical stylings of our most talented composer. This will be her first game composition, but J2 is no stranger to music, in fact you may have even seen her on tour with one of her bands!",
+        F: "Yo no sé que debo poner aquí.",
+    };
+    const imageMap = {
+        A: 'img/team/circleA.png',
+        B: 'img/team/circleB.png',
+        C: 'img/team/circleC.png',
+        D: 'img/team/circleD.png',
+        E: 'img/team/circleE.png',
+        F: 'img/team/circleF.png',
+    };
+
+    // Add change event listener to each radio button
+    radioButtons.forEach((radio) => {
+        radio.addEventListener("click", function () {
+            if (radio.checked) {
+                dynamicText.textContent = textMap1[radio.id];
+                dynamicText2.textContent = textMap2[radio.id];
+                dynamicText2.style.display = "block";
+                dynamicImage.src=imageMap[radio.id];
+            }
+        });
+    });
+});
